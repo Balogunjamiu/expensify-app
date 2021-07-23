3da594d7c7e68b55703316e69df47a3379a3b4f7
balogunjamiu49@gmail.com <balogunjamiu49@gmail.com>
2021-07-23 05:37:04 +0100

    setting up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path')
 const MiniCssExtractPlugin  = require('mini-css-extract-plugin')
 const  webpack = require('webpack')
-//process .env.Node
-process.env_NODE_ENV = process.env_NODE_ENV || 'development'
-if (process.env_NODE_ENV === 'test'){
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+if (process.env.NODE_ENV === 'test'){
     require('dotenv').config({path: '.env.test'})
-} else if(process.env_NODE_env === 'devlopment'){
+} else if(process.env.NODE_ENV === 'development'){
     require('dotenv').config({path: '.env.development'})
 }
 module.exports = (env, argv)=>{
@@ -45,13 +45,14 @@ module.exports = (env, argv)=>{
                 filename: 'styles.css'
             }),
             new webpack.DefinePlugin({
-                'process.env.FIREBASE_KEY' : JSON.stringify( process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_AUTH_DOMAIN' : JSON.stringify( process.env.FIREBASE_AUTH_DOMAIN),
-                'process.env.FIREBASE_PROJECT_ID' : JSON.stringify( process.env.FIREBASE_PROJECT_ID),
-                'process.env.FIREBASE_STORAGE_BUCKET' : JSON.stringify( process.env.FIREBASE_STORAGE_BUCKET),
-                'process.env.FIREBASE_MESSAGING_SENDER_ID' : JSON.stringify( process.env.FIREBASE_MESSAGING_SENDER_ID),
-                'process.env.FIREBASE_APP_ID' : JSON.stringify( process.env.FIREBASE_APP_ID),
-                'process.env.FIREBASE_MEASEUREMENT_ID' : JSON.stringify( process.env.FIREBASE_MEASEUREMENT_ID)
+                'process.env.FIREBASE_API_KEY' : JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_AUTH_DOMAIN' : JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.env.FIREBASE_DATABASE_URL' : JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.env.FIREBASE_PROJECT_ID' : JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET' : JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID' : JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+                'process.env.FIREBASE_APP_ID' : JSON.stringify(process.env.FIREBASE_APP_ID),
+                'process.env.FIREBASE_MEASEUREMENT_ID' : JSON.stringify(process.env.FIREBASE_MEASEUREMENT_ID)
 
             })
         ],
